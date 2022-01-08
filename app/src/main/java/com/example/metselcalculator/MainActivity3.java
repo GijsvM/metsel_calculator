9969a4478c3620e31a4c15ebbc25c4dceaedc84e
SharkieCodes <sharkiecodes@gmail.com>
2022-01-08 16:37:08 +0100

    error screen + rounding numbers

---
+++ app/src/main/java/com/example/metselcalculator/MainActivity3.java
@@ -0,0 +1,26 @@
+package com.example.metselcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity3 extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  new Intent(MainActivity3.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
